5f22d84a3d297965d62595a50a762ce8319237b8
John <john.usa.james@gmail.com>
2025-12-19 14:59:20 +0000

    feat: update environment variable names for Sanity projectId and dataset

--- apps/studio/sanity.config.ts
+++ apps/studio/sanity.config.ts
@@ -8,8 +8,8 @@ import { schemaTypes } from "./schema-types";
 export default defineConfig({
   name: "default",
   title: "Blog",
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID ?? "gq0n5o36",
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET ?? "development",
+  projectId: process.env.SANITY_STUDIO_SANITY_PROJECT_ID ?? "gq0n5o36",
+  dataset: process.env.SANITY_STUDIO_SANITY_DATASET ?? "development",
   plugins: [codeInput(), structureTool(), visionTool()],
   schema: {
     types: schemaTypes,
